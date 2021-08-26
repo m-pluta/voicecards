54557578814a7e38d105f50b835cb3c70388fb11
Michal Pluta <michalpl2003@gmail.com>
2021-08-26 14:57:45 +0100

    Code for the 'Reveal password' button now works

--- app/src/main/java/com/thundercandy/epq/LoginActivity.java
+++ app/src/main/java/com/thundercandy/epq/LoginActivity.java
@@ -1,14 +1,32 @@
 package com.thundercandy.epq;
 
+import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputLayout passwordField = (TextInputLayout) findViewById(R.id.PasswordField);
+        passwordField.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+                if (txtPassword.getTransformationMethod() == null) {
+                    txtPassword.setTransformationMethod(new PasswordTransformationMethod());
+                } else {
+                    txtPassword.setTransformationMethod(null);
+                }
+            }
+        });
     }
 }
