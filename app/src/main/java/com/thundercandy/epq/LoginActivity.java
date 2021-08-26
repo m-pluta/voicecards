8d5f389d4bead7d1b31951e261a8e9614db5c1bb
Michal Pluta <michalpl2003@gmail.com>
2021-08-26 15:45:50 +0100

    Added a button for signing in - not working yet and some text which acts as a button but allows the user to register an account Used SpannableStrings to make some of the text in a TextView bold and red and some of it as default

--- app/src/main/java/com/thundercandy/epq/LoginActivity.java
+++ app/src/main/java/com/thundercandy/epq/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.thundercandy.epq;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.method.PasswordTransformationMethod;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,16 +25,33 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.activity_login);
 
         TextInputLayout passwordField = (TextInputLayout) findViewById(R.id.PasswordField);
-        passwordField.setEndIconOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
-                if (txtPassword.getTransformationMethod() == null) {
-                    txtPassword.setTransformationMethod(new PasswordTransformationMethod());
-                } else {
-                    txtPassword.setTransformationMethod(null);
-                }
+        passwordField.setEndIconOnClickListener(v -> {
+            EditText txtPassword = (EditText) findViewById(R.id.txtPassword);
+            if (txtPassword.getTransformationMethod() == null) {
+                txtPassword.setTransformationMethod(new PasswordTransformationMethod());
+            } else {
+                txtPassword.setTransformationMethod(null);
             }
         });
+
+        setupRegisterButtonUI();
+
+
+    }
+
+    private void setupRegisterButtonUI() {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        SpannableString ss = new SpannableString(getString(R.string.no_account_register));
+
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        ForegroundColorSpan fcs = new ForegroundColorSpan(getResources().getColor(R.color.defaultRed_100));
+
+        ss.setSpan(boldSpan, 23, 31, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(fcs, 23, 31, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        builder.append(ss);
+
+        TextView textView = (TextView) findViewById(R.id.NoAccount_Register);
+        textView.setText(ss);
     }
 }
