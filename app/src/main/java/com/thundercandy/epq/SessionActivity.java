9a9c975b84bee15d4cfbfccbb80cc181df581b78
Michal Pluta <michalpl2003@gmail.com>
2021-08-31 02:58:34 +0100

    Slight layout changes to update the background of SessionActivity Added animation for moving between HomeActivity and SessionActivity

--- app/src/main/java/com/thundercandy/epq/SessionActivity.java
+++ app/src/main/java/com/thundercandy/epq/SessionActivity.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 public class SessionActivity extends AppCompatActivity {
 
+    ImageView btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
+        btnBack = findViewById(R.id.btnBack);
+
+        overridePendingTransition(R.anim.forward_slide_in, R.anim.forward_slide_out);
+
+        btnBack.setOnClickListener(v -> {
+            onBackPressed();
+        });
 
         Intent receivedIntent = getIntent();
 
@@ -25,5 +35,17 @@ public class SessionActivity extends AppCompatActivity {
 
 
         return cards;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.back_slide_in, R.anim.back_slide_out);
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.back_slide_in, R.anim.back_slide_out);
     }
 }
