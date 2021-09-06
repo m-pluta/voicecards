8d19a154be96cb122044fe6403c498f6f1ac1c76
Michal Pluta <michalpl2003@gmail.com>
2021-09-06 01:39:47 +0100

    Remodelled the code for starting different timers

--- app/src/main/java/com/thundercandy/epq/PomodoroActivity.java
+++ app/src/main/java/com/thundercandy/epq/PomodoroActivity.java
@@ -7,21 +7,22 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class PomodoroActivity extends DrawerActivity {
 
-    private static final int DEFAULT_INTERVAL = 1000;                   // One second
-    private static final int DEFAULT_DURATION = 30 * 60 * 1000;         // 30 minutes
+    private static final int DEFAULT_INTERVAL = 1000;                   // One second //TODO: allow smooth intervals
 
-    private long duration = DEFAULT_DURATION;
-    private long interval = DEFAULT_INTERVAL;
+    CountDownTimer timer;
 
     ImageView timerCircle;
     TextView txtTimeRemaining;
+    Button btnStartPomodoro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,56 +31,90 @@ public class PomodoroActivity extends DrawerActivity {
         setToolbarTitle("Pomodoro");
         timerCircle = findViewById(R.id.timerCircle);
         txtTimeRemaining = findViewById(R.id.txtTimeRemaining);
+        btnStartPomodoro = findViewById(R.id.btnStartPomodoro);
 
-        int strokeWidth = 6;
-        int halfStrokeWidth = strokeWidth / 2;
+        btnStartPomodoro.setOnClickListener(v -> {
+            startPomodoro();
+        });
+    }
 
-        CountDownTimer timer = new CountDownTimer(duration, interval) {
+    public void startPomodoro() {
+        changeTimerUIVisibility(true);
+
+        long duration = Utils.getPomodoroLength(this);
+        long interval = DEFAULT_INTERVAL;
+
+        timer = new CountDownTimer(duration, interval) {
             @Override
             public void onTick(long millisUntilFinished) {
+                updateProgressCircle((float) millisUntilFinished / duration);
                 String timeRemaining = Utils.getDurationBreakdown(millisUntilFinished);
                 txtTimeRemaining.setText(timeRemaining);
             }
 
             @Override
             public void onFinish() {
+                changeTimerUIVisibility(false);
+
                 Toast.makeText(PomodoroActivity.this, "Timer finished", Toast.LENGTH_SHORT).show();
+                //TODO: show break option or next pomodoro
             }
         };
         timer.start();
-
-        Thread myThread = new Thread() {
-
-            public void run() {
-
-                final Bitmap bitmap = Bitmap.createBitmap(200 + strokeWidth, 200 + strokeWidth, Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(bitmap);
-
-                int startingDeg = -90;
-                int deg = 0;
-                RectF rectF = new RectF(halfStrokeWidth, halfStrokeWidth, 200 + halfStrokeWidth, 200 + halfStrokeWidth);
-                Paint paint;
-                paint = new Paint();
-                paint.setColor(Color.GREEN);
-                paint.setStyle(Paint.Style.STROKE);
-                paint.setStrokeWidth(strokeWidth);
-
-
-                for (int i = 0; i <= 36; i++) {
-                    canvas.drawArc(rectF, startingDeg, deg, false, paint);
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-
-                    runOnUiThread(() -> timerCircle.setImageBitmap(bitmap));
-                    deg += 10;
-                }
-            }
-        };
-        myThread.start();
-
     }
 
+    public void startBreak() {
+        changeTimerUIVisibility(true);
+
+        long duration = Utils.getBreakLength(this);
+        long interval = DEFAULT_INTERVAL;
+
+        timer = new CountDownTimer(duration, interval) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateProgressCircle((float) millisUntilFinished / duration);
+                String timeRemaining = Utils.getDurationBreakdown(millisUntilFinished);
+                txtTimeRemaining.setText(timeRemaining);
+            }
+
+            @Override
+            public void onFinish() {
+                changeTimerUIVisibility(false);
+
+                Toast.makeText(PomodoroActivity.this, "Timer finished", Toast.LENGTH_SHORT).show();
+                //TODO: show next pomodoro
+            }
+        };
+        timer.start();
+    }
+
+    private void updateProgressCircle(float prg) {
+        int strokeWidth = 6;
+        int halfStrokeWidth = strokeWidth / 2;
+
+        final Bitmap bitmap = Bitmap.createBitmap(200 + strokeWidth, 200 + strokeWidth, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        float startingAngle = -90f;
+        float endingAngle = prg * 360f;
+
+        RectF rectF = new RectF(halfStrokeWidth, halfStrokeWidth, 200 + halfStrokeWidth, 200 + halfStrokeWidth);
+        Paint paint;
+        paint = new Paint();
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(strokeWidth);
+
+        canvas.drawArc(rectF, startingAngle, endingAngle, false, paint);
+        runOnUiThread(() -> timerCircle.setImageBitmap(bitmap));
+    }
+
+    public void changeTimerUIVisibility(boolean vis) {
+        FrameLayout timerUI = findViewById(R.id.timerUI);
+        if (vis) {
+            timerUI.setVisibility(View.VISIBLE);
+        } else {
+            timerUI.setVisibility(View.INVISIBLE);
+        }
+    }
 }
