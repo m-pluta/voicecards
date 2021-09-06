ab0ab853cec6a857aeee4176126dfaf0b60cf099
Michal Pluta <michalpl2003@gmail.com>
2021-09-06 01:02:25 +0100

    Added methods for fetching saved preferences

--- app/src/main/java/com/thundercandy/epq/Utils.java
+++ app/src/main/java/com/thundercandy/epq/Utils.java
@@ -12,6 +12,8 @@ import java.util.concurrent.TimeUnit;
 
 public class Utils {
 
+    public static final int UNIT_MINUTE = 60000;
+
     public static String capitalize(String str) {
         if (str == null || str.length() == 0) {
             return str;
@@ -32,36 +34,68 @@ public class Utils {
 
     public static double getLearntThreshold(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String KEY_learntThreshold = context.getResources().getString(R.string.KEY_learntThreshold);
-        int default_learntThreshold = context.getResources().getInteger(R.integer.default_learntThreshold);
+        String key = context.getResources().getString(R.string.KEY_learntThreshold);
+        int defValue = context.getResources().getInteger(R.integer.default_learntThreshold);
 
-        int temp = sharedPreferences.getInt(KEY_learntThreshold, default_learntThreshold);
+        int temp = sharedPreferences.getInt(key, defValue);
         return (double) temp / 100;
 
     }
 
     public static double getSeenThreshold(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String KEY_seenThreshold = context.getResources().getString(R.string.KEY_seenThreshold);
-        int default_seenThreshold = context.getResources().getInteger(R.integer.default_seenThreshold);
+        String key = context.getResources().getString(R.string.KEY_seenThreshold);
+        int defValue = context.getResources().getInteger(R.integer.default_seenThreshold);
 
-        return sharedPreferences.getInt(KEY_seenThreshold, default_seenThreshold);
+        return sharedPreferences.getInt(key, defValue);
     }
 
     public static boolean getAutoCapitalize(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String KEY_auto_capitalize = context.getResources().getString(R.string.KEY_auto_capitalize);
-        boolean default_auto_capitalize = context.getResources().getBoolean(R.bool.default_auto_capitalize);
+        String key = context.getResources().getString(R.string.KEY_auto_capitalize);
+        boolean defValue = context.getResources().getBoolean(R.bool.default_auto_capitalize);
 
-        return sharedPreferences.getBoolean(KEY_auto_capitalize, default_auto_capitalize);
+        return sharedPreferences.getBoolean(key, defValue);
     }
 
     public static boolean getAutoFullStop(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        String KEY_auto_full_stop = context.getResources().getString(R.string.KEY_auto_full_stop);
-        boolean default_auto_full_stop = context.getResources().getBoolean(R.bool.default_auto_full_stops);
+        String key = context.getResources().getString(R.string.KEY_auto_full_stop);
+        boolean defValue = context.getResources().getBoolean(R.bool.default_auto_full_stops);
 
-        return sharedPreferences.getBoolean(KEY_auto_full_stop, default_auto_full_stop);
+        return sharedPreferences.getBoolean(key, defValue);
+    }
+
+    public static long getPomodoroLength(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String key = context.getResources().getString(R.string.KEY_pomodoro_length);
+        int defValue = context.getResources().getInteger(R.integer.default_pomodoro_length);
+
+        return (long) sharedPreferences.getInt(key, defValue) * UNIT_MINUTE;
+    }
+
+    public static boolean getBreaks(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String key = context.getResources().getString(R.string.KEY_breaks);
+        boolean defValue = context.getResources().getBoolean(R.bool.default_breaks);
+
+        return sharedPreferences.getBoolean(key, defValue);
+    }
+
+    public static long getBreakLength(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String key = context.getResources().getString(R.string.KEY_break_length);
+        int defValue = context.getResources().getInteger(R.integer.default_break_length);
+
+        return (long) sharedPreferences.getInt(key, defValue) * UNIT_MINUTE;
+    }
+
+    public static boolean getBreaksAutoStart(Context context) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        String key = context.getResources().getString(R.string.KEY_breaks_auto_start);
+        boolean defValue = context.getResources().getBoolean(R.bool.default_breaks_auto_start);
+
+        return sharedPreferences.getBoolean(key, defValue);
     }
 
     public static void removeBottomNavigation(Activity activity) {
